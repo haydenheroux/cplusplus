a3c4a6ad2ec295d83f0a67d4ffed60d1e99fda0b
Hayden Heroux <haydenheroux@gmail.com>
2024-06-01 11:55:13 -0400

    Project name from arguments

--- project_generator/main.cpp
+++ project_generator/main.cpp
@@ -1,14 +1,42 @@
-#include <iostream>
-
 #include "project.hpp"
 
-int main() {
+int main(int argc, char* argv[]) {
     std::string project_name;
+    bool create_files = false;
+    bool update_build = false;
 
-    std::cin >> project_name;
+    for (int i = 1; i < argc; i++) {
+        std::string arg = std::string(argv[i]);
+
+        if (arg[0] == '-' && arg.length() == 2) {
+            switch (arg[1]) {
+                case 'c':
+                    create_files = !create_files;
+                    break;
+                case 'u':
+                    update_build = !update_build;
+                    break;
+                default:
+                    break;
+            }
+        } else {
+            project_name = arg;
+        }
+    }
+
+    if (project_name.length() == 0) {
+        return 1;
+    }
 
     project project{project_name};
 
-    project.create_files();
-    project.update_build();
+    if (create_files) {
+        project.create_files();
+    }
+
+    if (update_build) {
+        project.update_build();
+    }
+
+    return 0;
 }
